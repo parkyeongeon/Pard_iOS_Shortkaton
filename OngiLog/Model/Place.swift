3d5faed6acd90b4f4973d33fe00a57bd7e88c126
yeongeon <duddjs1646@handong.ac.kr>
2025-11-22 05:42:05 +0900

    Remove BaseURL.swift from tracking

---
+++ OngiLog/Model/Place.swift
@@ -0,0 +1,18 @@
+//
+//  Place.swift
+//  OngiLog
+//
+//  Created by Taemin KIM on 11/22/25.
+//
+
+import SwiftUI
+
+struct Place: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Place()
+}
