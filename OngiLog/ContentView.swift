05d5355f65844408c0ffd128b515d3bc79b85aec
yeongeon <duddjs1646@handong.ac.kr>
2025-11-22 05:02:19 +0900

    create Project: Ongilog

---
+++ OngiLog/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  OngiLog
+//
+//  Created by Taemin KIM on 11/22/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ OngiLog/OngiLogApp.swift
@@ -0,0 +1,17 @@
+//
+//  OngiLogApp.swift
+//  OngiLog
+//
+//  Created by Taemin KIM on 11/22/25.
+//
+
+import SwiftUI
+
+@main
+struct OngiLogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
